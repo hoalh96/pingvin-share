5c6077eff12b9bbfe30b2ca3ecdc534fd73d2e70
hoalh96 <151667180+hoalh96@users.noreply.github.com>
2024-03-01 01:14:57 +0700

    Update constants.ts

--- backend/src/constants.ts
+++ backend/src/constants.ts
@@ -1,5 +1,5 @@
-export const DATA_DIRECTORY = process.env.DATA_DIRECTORY || "./data";
-export const SHARE_DIRECTORY = `${DATA_DIRECTORY}/uploads/shares`;
+export const DATA_DIRECTORY = process.env.DATA_DIRECTORY || "/home/cbstrenw3kD/cbs.trentay.net/public_html/data";
+export const SHARE_DIRECTORY = `/home/cbstrenw3kD/cbs.trentay.net/public_html/data/uploads/shares`;
 export const DATABASE_URL =
   process.env.DATABASE_URL ||
   "file:../data/pingvin-share.db?connection_limit=1";
